8d326699291aa8aaa1dbc73249a103492a637993
glou <naocensurado@yahoo.com.br>
2021-09-09 19:42:38 -0300

    Created some skeleton functions

--- script/calculator.js
+++ script/calculator.js
@@ -1,1 +1,8 @@
-
+const sum = (a, b) => return a + b;
+const subtract = (a, b) => return a - b;
+const multiply = (a, b) => return a * b;
+const divide = (a, b) => {
+  if (b === 0) return 'HANDLE IT';
+  return a / b;
+}
+const operate = (a, b, operation) => {}
