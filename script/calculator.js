6ece50f12de60e0aee9ff967de6e5fa99af79828
Gabriel <naocensurado@yahoo.com.br>
2021-09-11 13:59:44 -0300

    CSS file

--- script/calculator.js
+++ script/calculator.js
@@ -1,8 +1,15 @@
-const sum = (a, b) => return a + b;
-const subtract = (a, b) => return a - b;
-const multiply = (a, b) => return a * b;
+const sum = (a, b) => {return a + b};
+const subtract = (a, b) => {return a - b};
+const multiply = (a, b) => {return a * b};
 const divide = (a, b) => {
   if (b === 0) return 'HANDLE IT';
   return a / b;
 }
-const operate = (a, b, operation) => {}
+const operate = (a, b, operation) => {
+  switch (operation) {
+    case '+': return sum(a, b);
+    case '-': return subtract(a, b);
+    case '*': return multiply(a, b);
+    case '/': return divide(a, b);
+  }
+}
