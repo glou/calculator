ec93a23398a73d9f8fd8bbac1f73387ff5c20053
Gabriel <naocensurado@yahoo.com.br>
2021-09-17 03:15:35 -0300

    Done keyboard support and floating point. I will finish with instructions for the user

--- script/calculator.js
+++ script/calculator.js
@@ -58,16 +58,19 @@ const holdInfo = (e) => {
     operation = e.target.value;
     return;
   }
+  if (pointBtn.disabled === true) {
+    pointBtn.disabled = false;
+  }
   if (firstValue === null || flag === 'equal') {
     firstValue = +display.textContent;
     operation = e.target.value;
   } else {
     secondValue = +display.textContent;
-    secondValue = operate(firstValue, secondValue, operation); //Executes an operation when there're 2 values
+    secondValue = Math.round(operate(firstValue, secondValue, operation) * 100000) / 100000; //Executes an operation when there're 2 values, up to 5 decimal places
     if (secondValue === 'err') return;
     display.textContent = secondValue;
     firstValue = secondValue;    
-    operation = e.target.value;
+    operation = e.target.value;    
   }  
   flag = 'operation';
 }
@@ -92,34 +95,114 @@ const displayNumbers = (e) => {
 const numbersBtn = Array.from(document.getElementsByClassName('number-btn'));
 numbersBtn.forEach(element => {element.addEventListener('click', displayNumbers)});
 
+//Callback for floating point
+const pointBtn = document.getElementById('point');
+pointBtn.addEventListener('click', () => {
+  if (pointBtn.disabled === false) {
+    display.textContent += '.';
+    pointBtn.disabled = true;
+  }
+
+});
+
 const operationsBtn = Array.from(document.getElementsByClassName('operation-btn'));
 operationsBtn.forEach(element => {element.addEventListener('click', holdInfo)});
 
 
 //Callback for clearing the display
-document.getElementById('clear-btn').addEventListener('click', () => {
+const clearBtn = document.getElementById('clear-btn');
+clearBtn.addEventListener('click', () => {
   firstValue = null;
   operation = null;
   flag = 'number';
+  pointBtn.disabled = false;
   display.textContent = '';
 })
 
 //Callback for backspace button
-document.getElementById('bksp-btn').addEventListener('click', () => {
+const bkspBtn = document.getElementById('bksp-btn');
+bkspBtn.addEventListener('click', () => {
   display.textContent = display.textContent.slice(0, display.textContent.length - 1)
 })
 
 //Callback for the 'equal' button
-document.getElementById('equal-btn').addEventListener('click', () => {
+const equalBtn = document.getElementById('equal-btn');
+equalBtn.addEventListener('click', () => {
   if (display.textContent === '' || operation === null) {
     return;
   } 
   secondValue = +display.textContent;
-  secondValue = operate(firstValue, secondValue, operation); //Executes an operation when there're 2 values
+  secondValue = Math.round(operate(firstValue, secondValue, operation) * 100000) / 100000; //Executes an operation when there're 2 values, up to 5 decimal places
   if (secondValue === 'err') return;
   display.textContent = secondValue;
   firstValue = secondValue;    
   operation = null;
-  flag = 'equal';  
+  flag = 'equal';
+  if (display.textContent.indexOf('.') != -1) {
+    pointBtn.disabled = true;
+  }
 })
+
+//Keyboard support
+const captureKeys = (e) => {
+  switch (e.key) {
+    case '0':
+      numbersBtn[0].click();
+      break;    
+    case '1':
+      numbersBtn[1].click();
+      break;
+    case '2':
+      numbersBtn[2].click();
+      break;
+    case '3':
+      numbersBtn[3].click();
+      break;
+    case '4':
+      numbersBtn[4].click();
+      break;
+    case '5':
+      numbersBtn[5].click();
+      break;
+    case '6':
+      numbersBtn[6].click();
+      break;
+    case '7':
+      numbersBtn[7].click();
+      break;
+    case '8':
+      numbersBtn[8].click();
+      break;
+    case '9':
+      numbersBtn[9].click();
+      break;
+    case '+':
+      operationsBtn[0].click();
+      break;
+    case '-':
+      operationsBtn[1].click();
+      break;
+    case '*':
+      operationsBtn[2].click();
+      break;
+    case '/':
+      operationsBtn[3].click();
+      break;
+    case '=':
+      equalBtn.click();
+      break;
+    case '.':
+      pointBtn.click();
+      break;
+    case 'Backspace':
+      bkspBtn.click();
+      break;
+    case 'c':
+    case 'C':
+      clearBtn.click();
+      break;
+  }  
+}
+
+document.addEventListener('keydown', captureKeys)
 
